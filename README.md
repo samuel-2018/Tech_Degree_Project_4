9a2156c2cd7c14ca88e1490f8e15ba3ed5cfcd67
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-06-24 10:58:31 -0400

    first commit, added starter files and phrases

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tech_Degree_Project_4
+
+Project 4: OOP Game Show App (Tech Degree)
+
+## Description
+
+## Main Features
+
+## “Extra Credit” Features
---
+++ js/Game.js
@@ -0,0 +1,16 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      'Some are born great some achieve greatness and some have greatness thrust upon them',
+      'Brevity is the soul of wit',
+      'Just think happy thoughts and you’ll fly',
+      'Romeo Romeo wherefore art thou Romeo',
+      'Not all those who wander are lost',
+    ];
+    this.activePhrase = null;
+  }
+}
