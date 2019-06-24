a523d6d9401491f838204c85b969078d4cdfce23
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-06-24 14:48:51 -0400

    part 1 completed, event listener for 'start game'

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+let game;
+document.querySelector('#btn__reset').addEventListener('click', () => {
+  game = new Game();
+  game.startGame();
+});
